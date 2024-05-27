1af1ae5dda1aa83601327f7adc8029e9a7c890d2
Alexandria Pagram <alexandriagriffiths@yahoo.com.au>
2024-05-27 13:39:25 +1000

    Update PlayerPatch.cs

--- Hooks/PlayerPatch.cs
+++ Hooks/PlayerPatch.cs
@@ -7,22 +7,22 @@ public class PlayerPatch
 {
     internal static void Init()
     {
-        // Dev Note: SurfaceNetworkHandler.InitSurface runs after PhotonGameLobbyHandler.ReturnToSurface
         On.Player.Start += Player_Start;
     }
 
-    // When returning from spelunking,
-    // Must wait until camera.main and players exist before running rewards (or SFX that plays when UI message is shown will fail and wreak havoc) 
-
+    // When returning from spelunking, must wait until camera.main and players exist before running rewards 
+    // (or SFX that plays when UI message is shown will fail and wreak havoc) 
+    // so we run the code here on Player.Start()
     private static IEnumerator Player_Start(On.Player.orig_Start orig, Player self)
     {
-        var value = orig(self);
+        var returnValue = orig(self);
 
         // TODO - just use the hosts' values for everyone.
 
         if (self.IsLocal)
         {
-            // if we respawn after spelunking underground and there is a pending reward for our safe return of the camera
+            // if we respawn after spelunking underground,
+            // check if there is a pending reward for our safe return of the camera
             if (TimeOfDayHandler.TimeOfDay == TimeOfDay.Evening
                 && KeepCameraAfterDeath.Instance.IsRewardForCameraReturnEnabled
                 && KeepCameraAfterDeath.Instance.PendingRewardForCameraReturn)
@@ -33,7 +33,7 @@ public class PlayerPatch
             }
         }
 
-        return value;
+        return returnValue;
 
 
         void AddCashToRoom()
