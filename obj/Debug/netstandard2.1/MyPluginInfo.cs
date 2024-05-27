c1855f8f64ea39ab12ead10bedddbf1ecdd876c4
Alexandria Pagram <alexandria@pagr.am>
2024-05-27 11:49:03 +1000

    remove vs files

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace KeepCameraAfterDeath
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "alexandria-p.KeepCameraAfterDeath";
-        public const string PLUGIN_NAME = "KeepCameraAfterDeath";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
